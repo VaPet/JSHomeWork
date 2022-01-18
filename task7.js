9dbe9d33679d6920acfadf91b0a8baa8dbe6e524
VaPet <va.petriaiev@gmail.com>
2022-01-18 10:30:17 +0200

    Update task7.js

--- task7.js
+++ task7.js
@@ -15,7 +15,9 @@ var arr = [1,2,3,4];
 var arr = [1, 2, 3, 4];
 
 var sum = 0;
-for (var i = 0; i < arr.length; i++) 
-    sum += arr[i];
+for (var i = 0; i < arr.length; i++) {
+    if (i % 2 == 0) {
+        sum += arr[i];
+    }
     
 console.log(sum);
