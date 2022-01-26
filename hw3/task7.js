1bfe376f05b3760643e32ef9a4bd3bbcff614aa0
vadym petriaiev <va.petryaev@gmail.com>
2022-01-26 12:44:38 +0200

    26.01.2022

---
+++ hw3/task7.js
@@ -0,0 +1,24 @@
+/*
+####Задача 7
+
+Код ниже получает из массива строк новый массив, содержащий их длины:
+
+```javascript
+var arr = ['abcd', 'abcde', 'ab', 'abc'];
+
+var arrLength = [];
+for (var i = 0; i < arr.length; i++) {
+arrLength[i] = arr[i].length;
+}
+
+console.log( arrLength ); // 4,5,2,3
+```
+
+Перепишите выделенный участок:
+
+- используйте вместо цикла `for` метод `map`
+*/
+
+let arr = ['abcd', 'abcde', 'ab', 'abc'];
+var arrLength = arr.map(x => x.length);
+console.log(arrLength);
